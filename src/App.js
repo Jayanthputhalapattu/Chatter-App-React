2f37c82cb1682f36b5aec55098a21ebdda072fe8
JAYANTH PUTHALAPATTU <45038110+Jayanthputhalapattu@users.noreply.github.com>
2020-11-12 10:14:18 +0530

    UI changes many changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import BottomBar from "./BottomBar";
 import "./App.css";
 import { RespContext } from "./context/RespContext";
 import axios from "axios";
+import { Container, Row, Col } from "reactstrap";
 const App = () => {
   const [chat, setChat] = useState([]);
 
@@ -13,24 +14,51 @@ const App = () => {
     axios.get("https://u6o0u.sse.codesandbox.io/message").then((resp) => {
       setChat(resp.data);
     });
-    var x = window.innerHeight;
-    // console.log(x);
-    window.scrollTo(0, x);
+    window.scrollTo(0, window.innerHeight);
   });
 
   return (
     <RespContext.Provider value={{ chat, setChat }}>
-      <div className="App">
-        {chat.map((res, index) => (
-          <div className="message-box">
-            <ul style={{ listStyleType: "none" }}>
-              <li style={{ fontSize: 13 }}>{res.name}</li>
-              <li style={{ fontSize: 22, paddingLeft: 20 }}>{res.content}</li>
-            </ul>
-          </div>
-        ))}
-        <BottomBar />
-      </div>
+      <Container className="App" fluid>
+        <Row>
+          <ul style={{ listStyleType: "none" }}>
+            {chat.map((res, index) => (
+              <div className="message-box">
+                <li
+                  style={{
+                    fontSize: 13,
+                    color: "#2B2B52",
+                    paddingLeft: 20,
+                    fontFamily: "Montserrat, sans-serif",
+                    fontWeight: "bold"
+                  }}
+                >
+                  {res.name}
+                </li>
+                <li
+                  style={{
+                    fontSize: 15,
+                    paddingLeft: 20,
+                    fontFamily: "Montserrat, sans-serif",
+                    color: "white",
+                    fontWeight: "bold"
+                  }}
+                >
+                  {res.content}
+                </li>
+              </div>
+            ))}
+          </ul>
+        </Row>
+      </Container>
+      <Container fluid>
+        <Row>
+          <Col style={{ marginTop: 15 }}>
+            <BottomBar />
+          </Col>
+        </Row>
+      </Container>
+      <div></div>
     </RespContext.Provider>
   );
 };
