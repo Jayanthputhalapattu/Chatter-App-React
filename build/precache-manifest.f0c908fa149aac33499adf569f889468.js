c02c910464ecf18071246cc0c513abcc8890978f
Jayanth <jayanthp17@gmail.com>
2020-11-17 18:08:01 +0530

    suppressed warninig

---
+++ build/precache-manifest.f0c908fa149aac33499adf569f889468.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36911202887e8c89e0cf5f85e66e99d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a71b6a6eaf6e5b3342a",
+    "url": "/static/css/2.c9e7500d.chunk.css"
+  },
+  {
+    "revision": "2cc469e4c4168367657c",
+    "url": "/static/css/main.dffa43f1.chunk.css"
+  },
+  {
+    "revision": "2a71b6a6eaf6e5b3342a",
+    "url": "/static/js/2.9334778e.chunk.js"
+  },
+  {
+    "revision": "6d6cf611105a83a405709e467e0d2311",
+    "url": "/static/js/2.9334778e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc469e4c4168367657c",
+    "url": "/static/js/main.3769c265.chunk.js"
+  },
+  {
+    "revision": "d79e5a544bc277e4eb30",
+    "url": "/static/js/runtime-main.6edaf502.js"
+  }
+]);
